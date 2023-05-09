e25b2f405bd21dff22c5eb06986f85df10055d35
purnatag <purnatag@gmail.com>
2023-05-09 11:19:51 +0530

    Changes to be committed: 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -19,19 +19,20 @@ fn push(x : i32, mut stream: &[i32], mut n : i32, mut stats: &[f64]) ->  &[i32]{
     return &mut newstream;
 }
 */
-fn compute_stats(arr: & [i32]) -> [f64;4]{
-    let n = arr.len();
-    let mut stats:[f64;4] = [0.0;4];
-    let delta = (arr[n-1] as f64) - stats[0];
-    let delta_n = delta/(n as f64) ;
+fn compute_stats(i: usize, arr: & [f64], stats: &mut [f64;4]) -> [f64;4]{
+    let s = arr.len();
+    let n = (i as f64) + 1.0;
+    let delta = arr[s-i-1] - stats[0];
+    let delta_n = delta/n ;
     let delta_n2 = delta_n * delta_n ;
-    let term1 = delta * delta_n * delta_n2;
+    let term1 = delta * delta_n * (i as f64);
     stats[0] += delta_n;
-    stats[3] += term1*delta_n2*((n*n - 3*n + 3) as f64) + 6.0*delta_n2 *stats[1] - 4.0*delta_n*stats[2];
-    stats[2] += term1*delta_n*((n - 2) as f64) - 3.0*delta_n*stats[1];
+    stats[3] += term1*delta_n2*(3.0 + n*n - 3.0*n) + 6.0*delta_n2*stats[1] - 4.0*delta_n*stats[2];
+    stats[2] += term1*delta_n*(n - 2.0) - 3.0*delta_n*stats[1];
     stats[1] += term1;
+    println!("Curr mean: {}", stats[0]);
 
-    return stats;
+    return  *stats;
 }
 
 fn get_mean(stats:&mut [f64]) -> f64{
@@ -47,7 +48,7 @@ fn get_sk(n:i32, stats:&mut [f64]) -> f64{
 }
 
 fn get_kurt(n:i32, stats:&mut [f64]) -> f64{
-    return ((n as f64)*stats[1])/(stats[2]*stats[2] - 3.0);
+    return ((n as f64)*stats[3])/(stats[2]*stats[2] - 3.0);
 }
 
 fn main() {
@@ -78,20 +79,21 @@ fn main() {
     }*/
 
 
-    let mut arr:[i32;4] = [0;4];
+    let mut arr:[f64;4] = [0.0;4];
     let n = arr.len();
-    let val = n as i32;
+    //let val = n as f64;
     let mut stats: [f64;4] = [0.0;4];
 
     for i in 0..4{
-        arr[n-i-1] = (n-i) as i32;
-        stats = compute_stats(&arr);
+        arr[n-i-1] = (n-i) as f64;
+        stats = compute_stats(i,&arr, &mut stats);
+        println!("index: {} val: {}", n-i-1, arr[n-i-1]);
     }
 
     println!("Current stream stats:");
     println!("Mean:{}", get_mean(&mut stats));
-    println!("Variance:{}", get_var(val, &mut stats));
-    println!("Skewness:{}", get_sk(val, &mut stats));
-    println!("Kurtosis:{}", get_kurt(val, &mut stats));
+    println!("Variance:{}", get_var(n as i32, &mut stats));
+    println!("Skewness:{}", get_sk(n as i32, &mut stats));
+    println!("Kurtosis:{}", get_kurt(n as i32, &mut stats));
 }
 
